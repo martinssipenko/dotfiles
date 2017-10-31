1fcd05b927f17df8dc51db044cae90cedbb63a7e
Martins Sipenko <martins.sipenko@gmail.com>
2017-10-31 09:32:05 +0200

    wip

--- init/60_sublime.sh
+++ init/60_sublime.sh
@@ -1,3 +1,6 @@
 
 rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -s ~/.dotfiles/conf/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s ~/.dotfiles/conf/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -s ~/.dotfiles/conf/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
