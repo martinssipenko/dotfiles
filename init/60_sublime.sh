67aa6b23c8752effa9c860422a82024b16696347
Martins Sipenko <martins.sipenko@gmail.com>
2019-07-19 12:51:11 +0300

    sublime goto_definition

--- init/60_sublime.sh
+++ init/60_sublime.sh
@@ -4,3 +4,6 @@ ln -s ~/.dotfiles/conf/Preferences.sublime-settings ~/Library/Application\ Suppo
 
 rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -s ~/.dotfiles/conf/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-mousemap
+ln -s ~/.dotfiles/conf/Default\ \(OSX\).sublime-mousemap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
