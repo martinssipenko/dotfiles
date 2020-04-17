38779734bb5f09d59f97490b2d4d64b6974e1ecc
Martins Sipenko <martins.sipenko@gmail.com>
2020-04-17 17:16:10 +0300

    fix typo

--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -6,9 +6,9 @@ is_osx || return 1
 
 # Ensure the cask kegs are installed.
 kegs=(
-  homebrew/casl-cask
-  homebrew/casl-drivers
-  homebrew/casl-fonts
+  homebrew/cask-cask
+  homebrew/cask-drivers
+  homebrew/cask-fonts
 )
 brew_tap_kegs
 
