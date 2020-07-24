02ea76c5c251898be06f064e7921085292ff68d8
Martins Sipenko <martins.sipenko@gmail.com>
2020-07-24 10:53:49 +0300

    display aws profile in prompt

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -106,9 +106,12 @@ fi;
 # Set the terminal title and prompt.
 PS1="\[\033]0;\W\007\]"; # working directory base name
 PS1+="\[${bold}\]\n"; # newline
-PS1+="\[${userStyle}\]\u"; # username
-PS1+="\[${white}\] at ";
-PS1+="\[${hostStyle}\]\h"; # host
+# PS1+="\[${userStyle}\]\u"; # username
+# PS1+="\[${white}\] at ";
+# PS1+="\[${hostStyle}\]\h"; # host
+PS1+="☁️  " # cloud emoji
+PS1+="\[${green}\]"
+PS1+="\$(echo \${AWS_PROFILE:-default})"
 PS1+="\[${white}\] in ";
 PS1+="\[${green}\]\w"; # working directory full path
 PS1+="\$(prompt_git \"\[${white}\] on \[${violet}\]\" \"\[${blue}\]\")"; # Git repository details
